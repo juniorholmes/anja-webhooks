02dfa40511410c0f94eaa4f03d5b8ddb3f75defc
Junior <junior@holmesdigital.com.br>
2022-10-01 08:16:51 -0300

    Gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,6 @@
-import logging
-import os
-import gunicorn
 from flask import Flask, request, current_app
 
-from config import Config
-
-
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
 
     from app.api import bp as api_bp
